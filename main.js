37c0e6ba1abb77037ebe498269fad15e3c7be168
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2023-09-20 19:25:04 +0700

    delete Menu in main.js

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWin;
 
+// Listen for app to be ready
 app.on('ready', function(){
-    //create new window
-    mainWin = new BrowserWindow({
-        width: 1000,
-        height: 750
+    //open window
+    mainWin = new BrowserWindow({        
+        width: 1000, // Set the width of the window
+        height: 750, // Set the height of the window
+        resizable: false
     });
-    //load html file
     mainWin.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWin.html'),
-        protocol: 'file',
+        protocol: 'file:',
         slashes: true
     }));
+
+    const template = [];
+
+    //Build menu template
+    const mainMenu = Menu.buildFromTemplate(template);
+    // const mainMenu = Menu.buildFromTemplate([{label: 'More'}]);
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
+    
+    //Set background color
+    mainMenu.backgroundColor = mainWin.backgroundColor;
 });
 
