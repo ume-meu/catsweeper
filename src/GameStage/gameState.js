b6948a8c985a15e03e13141b855d19383a046260
nguyenthiyenchi <nguyenthiyenchi2k3@gmail.com>
2023-12-14 20:25:11 +0700

    rewrite game-container.id in js

--- src/GameStage/gameState.js
+++ src/GameStage/gameState.js
@@ -1,47 +1,51 @@
 // catsweeper
-var levels = {
-    easy:       {
-        id: 0,
-        rows: 9,
-        cols: 9,
-        cats: 10
-    },
-    medium:     {
-        id: 1,
-        rows: 16,
-        cols: 16,
-        cats: 40        
-    },
-    hard:       {
-        id: 2,
-        rows: 30,
-        cols: 16,
-        cats: 99      
-    }, 
-    extreme:    {
-        id: 3,
-        rows: 30,
-        cols: 24,
-        cats: 180    
-    },
-    custom:     {
-        id: 4,
-        minRows: 9,
-        minCols: 9,
-        minCats: 10,
-        maxRows: 30, 
-        maxCols: 30,
-        maxCats: 160
-    }
-};
-var inLevels =  {
-    undo: 3,
-    defaultLevel: 'easy',
-    currentLevel: null,
-    nRows: null,
-    nCols: null,
-    nCats: null,
-    flags: ['covered', 'flag', 'question'] 
+// var levels = {
+//     easy:       {
+//         id: 0,
+//         rows: 9,
+//         cols: 9,
+//         cats: 10
+//     },
+//     medium:     {
+//         id: 1,
+//         rows: 16,
+//         cols: 16,
+//         cats: 40        
+//     },
+//     hard:       {
+//         id: 2,
+//         rows: 30,
+//         cols: 16,
+//         cats: 99      
+//     }, 
+//     extreme:    {
+//         id: 3,
+//         rows: 30,
+//         cols: 24,
+//         cats: 180    
+//     },
+//     custom:     {
+//         id: 4,
+//         minRows: 9,
+//         minCols: 9,
+//         minCats: 10,
+//         maxRows: 30, 
+//         maxCols: 30,
+//         maxCats: 160
+//     }
+// };
+// var inLevels =  {
+//     undo: 3,
+//     defaultLevel: 'easy',
+//     currentLevel: null,
+//     nRows: null,
+//     nCols: null,
+//     nCats: null,
+//     flags: ['covered', 'flag', 'question'] 
+// }
+var level = {
+    numRows: 0,
+    numCols: 0
 }
 
 function chooseMode() {
@@ -54,12 +58,61 @@ function choose(option) {
     //change game size depending on mode
     if (option.value != "custom")   {
         const selectedMode = option.value.split('x');
-        const width = selectedMode[0];
-        const height = selectedMode[1];
+        level.numCols = selectedMode[0];
+        level.numRows = selectedMode[1];
         const ingame = document.getElementById('ingame');
-        ingame.style.width = width*20 + 'px';
-        ingame.style.height = height*20 + 'px';
+        ingame.style.width = level.numCols*20 + 'px';
+        ingame.style.height = level.numRows*20 + 'px';
     }
     // 
     chooseMode();
 };
+
+var catsweeper = {
+    numRows: level.numRows,
+    numCols: level.numCols,
+    init: function(elementID)  {
+        // var self = this;
+        $("#" + elementID).append(
+            '<header class="logo-name">' +
+            '<h1 class="name"><i class="fas fa-paw"></i> CATSWEEPER <i class="fas fa-paw"></i></h1>' +
+            '</header>' +
+            '<div class="settings">' +
+                '<a href="#help"><i class="fas fa-question btn"></i></a>' +
+                '<div class="mode" id="mode">' +
+                    '<button onclick="chooseMode()">Easy 9x9 (10 cats)</button>' +
+                    '<i class="bx bxs-down-arrow arrow" onclick="chooseMode()"></i>' +
+                    '<div class="options" id="options">' +
+                        '<option value="9x9x10" onclick="choose(this)">Easy 9x9 (10 cats)</option>' +
+                        '<option value="16x16x40" onclick="choose(this)">Medium 16x16 (40 cats)</option>' +
+                        '<option value="30x16x99" onclick="choose(this)">Hard 30x16 (99 cats)</option>' +
+                        '<option value="30x30x180" onclick="choose(this)">Extreme 30x24 (180 cats)</option>' +
+                        '<option value="0x0x0" onclick="choose(this)">Custom...</option>' +
+                    '</div>' +
+                '</div>' +
+                '<a href="#setting"><i class="fas fa-gear btn"></i></a>' +
+            '</div>' +
+            '<div class="stats">' +
+                '<value class="nCats val" id="nCats">0</value>' +
+                '<icon class="game-icon" id="game-icon"> 😿😾 </icon>' +
+                '<value class="time val" id="time">0</value>' +
+            '</div>' +
+            '<div class="ingame" id="ingame">' +                
+            '</div>');
+        // $("#" + elementID).newgame();
+        // $("#ingame").newgame();
+        
+        document.getElementById("ingame").textContent = this.numRows;
+    },
+
+    
+
+
+    newgame: function() {       
+        document.getElementById("ingame").textContent = this.numRows;
+    },
+}
+
+$(document).ready(function() {
+	catsweeper.init('game-container');
+});
